c8742728f36432123eb14e3fd0410ac313e9d468
lizzzarrrd <polina.batkilina@gmail.com>
2022-04-16 23:00:22 +0300

    redacted give function

--- funcs.py
+++ funcs.py
@@ -57,28 +57,31 @@ class RandomThings(commands.Cog):
             await ctx.send('попробуй позже')
 
     @commands.command()
-    async def give(self, ctx, id, n):
-        # если n не int то округлять вниз
+    async def give(self, ctx, id, n: int):
         members = self.get_members()
+        if ctx.message.mentions:
+            id = ctx.message.mentions[0].id
+        id = int(id)
         if id in members:
-            if id.isnumeric():
-                if int(id) != ctx.message.author.id:
-                    embed = discord.Embed(colour=0x78ccf0, description=f'Вы передали {n} :coin: пользователю <@{id}>')
-                    embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-                    await ctx.send(f'{id}, {ctx.message.author.id}', embed=embed)
-                else:
-                    embed = discord.Embed(colour=0x78ccf0, description=f'Нельзя передать монетки самому себе')
-                    embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-                    await ctx.send(embed=embed)
+            if id != ctx.message.author.id:
+                embed = discord.Embed(colour=0x78ccf0, description=f'Вы передали {n} :coin: пользователю <@{id}>')
+                embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
+                await ctx.send(embed=embed)
             else:
-                id = ctx.message.mentions[0].id
-                if ctx.author.id != id:
-                    embed = discord.Embed(colour=0x78ccf0, description=f'Вы передали {n} :coin: пользователю <@{id}>!')
-                    embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-                    await ctx.send(embed=embed)
-                else:
-                    embed = discord.Embed(colour=0x78ccf0, description=f'Нельзя передать монетки самому себе')
-                    embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-                    await ctx.send(embed=embed)
+                embed = discord.Embed(colour=0x78ccf0, description=f'Нельзя передать монетки самому себе')
+                embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
+                await ctx.send(embed=embed)
         else:
-            await ctx.send('человек не на сервере')
+            embed = discord.Embed(colour=0x78ccf0,
+                                  description=f'<@{id}> нет на сервере.\n\nНе удастся передать монетки')
+            embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
+            await ctx.send(embed=embed)
+
+    @give.error
+    async def give_error(self, ctx, error):
+        if isinstance(error, discord.ext.commands.CommandError):
+            embed = discord.Embed(colour=0xff2e2e, title='Не удалось передать монтеки',
+                                  description='Укажите человека, его id или линк, а затем количество монеток,'
+                                              ' которое вы хотите передать')
+            await ctx.send(embed=embed)
+
