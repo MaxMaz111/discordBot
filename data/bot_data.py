ec4f638885ab8278fe38eaa9cd8b5a819b6c6591
MaxMaz111 <maxmaz260206@gmail.com>
2022-04-19 22:44:04 +0300

    main.py: updated creating BotData

--- data/bot_data.py
+++ data/bot_data.py
@@ -1,11 +1,13 @@
-from typing import Dict
+from typing import List
 
 from data.guild_data import GuildData
 
 
 class BotData:
-    def __init__(self, guild_id_to_data: Dict[str, GuildData]):
-        self.guild_id_to_data = guild_id_to_data
+    def __init__(self, guilds: List[GuildData]):
+        self.guild_id_to_data = {}
+        for guild in guilds:
+            self.guild_id_to_data[guild.guild_id] = guild
 
-    def get_members(self, guild_id: str):
+    def get_members(self, guild_id: int):
         return self.guild_id_to_data[guild_id].get_members()
