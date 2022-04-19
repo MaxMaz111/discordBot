827dabd7dafe8653aed29de1f5303a801ac210a3
MaxMaz111 <maxmaz260206@gmail.com>
2022-04-19 22:31:27 +0300

    bot_data stores dict[guild_id, guild_data]

--- bot_data.py
+++ bot_data.py
@@ -1,17 +1,3 @@
 class BotData:
-    def __init__(self, bot, guild_id):
-        self.bot = bot
-        self.guild_id = guild_id
-
-        self.guild = None
-        self.members = None
-
-    def get_guild(self):
-        if self.guild is None:
-            self.guild = self.bot.get_guild(self.guild_id)
-        return self.guild
-
-    def get_members(self):
-        if self.members is None:
-            self.members = list(map(lambda x: x.id, filter(lambda x: not x.bot, self.get_guild().members)))
-        return self.members
+    def __init__(self, guild_id_to_data):
+        self.guild_id_to_data = guild_id_to_data
