472a5414353bf2059698e60000994309dd7ff23c
MaxMaz111 <maxmaz260206@gmail.com>
2022-04-19 22:31:09 +0300

    Added guild_data.py

---
+++ data/guild_data.py
@@ -0,0 +1,19 @@
+class GuildData:
+    def __init__(self, bot, guild_id):
+        self.bot = bot
+        self.guild_id = guild_id
+
+        self.guild = None
+        self.members = None
+
+    def get_guild(self):
+        if self.guild is None:
+            self.guild = self.bot.get_guild(self.guild_id)
+        return self.guild
+
+    def get_members(self):
+        if self.members is None:
+            self.members = set(
+                map(lambda x: x.id, filter(lambda x: not x.bot, self.get_guild().members))
+            )
+        return self.members
