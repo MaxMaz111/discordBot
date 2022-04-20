bcab430287a5bbda52c596bf7cd06fb4a27f3811
MaxMaz111 <maxmaz260206@gmail.com>
2022-04-20 16:49:26 +0300

    DbData: added get_money() and set_money() methods

--- data/db_data.py
+++ data/db_data.py
@@ -1,5 +1,5 @@
 from data import db_session
-from data.models import Users
+from data.models import Users, Money
 
 
 class DbData:
@@ -7,16 +7,29 @@ class DbData:
         db_session.global_init(db_name)
         self.db_sess = db_session.create_session()
 
-    def get_user(self, discord_id: int, guild_id: int):
+    def get_user(self, discord_id: int, guild_id: int) -> Users:
         user = self.db_sess.query(Users).filter(Users.guild_id == guild_id, Users.discord_id == discord_id).first()
         if not user:
             user = self.add_user(discord_id, guild_id)
         return user
 
-    def add_user(self, discord_id: int, guild_id: int):
+    def add_user(self, discord_id: int, guild_id: int) -> Users:
         user = Users()
         user.discord_id = discord_id
         user.guild_id = guild_id
         self.db_sess.add(user)
         self.db_sess.commit()
         return user
+
+    def set_money(self, user: Users, amount: int) -> None:
+        money = Money()
+        money.user_id = user.id
+        money.balance = amount
+        self.db_sess.add(money)
+        self.db_sess.commit()
+
+    def get_money(self, user: Users) -> int:
+        user_money = self.db_sess.query(Money).filter(Money.user_id == user.id).first()
+        if not user_money:
+            self.set_money(user, 0)
+        return user_money.balance
