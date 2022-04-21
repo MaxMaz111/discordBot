83f9634e95350d8075346f92c520e3504868e9c8
MaxMaz111 <maxmaz260206@gmail.com>
2022-04-21 13:08:02 +0300

    DailyRewardCommands: reward uses DiscordUtils.show_embed

--- commands/DailyRewardCommands.py
+++ commands/DailyRewardCommands.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+from commands import DiscordUtils
 from data.bot_data import BotData
 
 
@@ -12,15 +13,17 @@ class DailyRewardCommands(commands.Cog):
     @commands.command()
     @commands.cooldown(1, 60 * 60 * 24, commands.BucketType.user)
     async def reward(self, ctx):
-        self.bot_data.update_money(ctx=ctx, delta=20)
+        self.bot_data.update_money(ctx=ctx, delta=self.daily_reward)
 
-        embed = discord.Embed(title=f"Вы успешно получили свою награду - {self.daily_reward} :coin:",
-                              colour=0x778899)
-        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-        await ctx.send(embed=embed)
+        await DiscordUtils.show_embed(
+            ctx=ctx,
+            title=f"Вы успешно получили свою награду - {self.daily_reward} :coin:",
+            colour=0x778899,
+        )
 
     @reward.error
     async def reward_error(self, ctx, error):
+        print(error.args)
         if isinstance(error, discord.ext.commands.CommandOnCooldown):
             embed = discord.Embed(title=f'Вы уже получали свою награду, приходите позже.', colour=0xff2e2e)
             await ctx.send(embed=embed)
