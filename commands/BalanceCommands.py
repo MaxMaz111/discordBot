41bd51fe052e96d5573877afbbe2280eb73e8839
MaxMaz111 <maxmaz260206@gmail.com>
2022-04-21 12:55:55 +0300

    BalanceCommands: 'give' uses DiscordUtils.showEmbed

--- commands/BalanceCommands.py
+++ commands/BalanceCommands.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+from commands import DiscordUtils
 from commands.MembersCommands import MemberCommands
 from data.bot_data import BotData
 
@@ -10,33 +11,49 @@ class BalanceCommands(commands.Cog):
         self.bot_data = data
         self.top_limit = top_limit
 
+    @staticmethod
+    def get_recipient_id(ctx, recipient_id_argument):
+        recipient_id = ctx.message.mentions[0].id if ctx.message.mentions else recipient_id_argument
+        return int(recipient_id)
+
+    @staticmethod
+    def get_sender(ctx):
+        return ctx.message.author
+
     @commands.command()
-    async def give(self, ctx, id, n: int):
-        members = self.bot_data.get_members(ctx)
-        if ctx.message.mentions:
-            id = ctx.message.mentions[0].id
-        id = int(id)
-        if id in members:
-            if id != ctx.message.author.id:
-                embed = discord.Embed(colour=0x78ccf0, description=f'Вы передали {n} :coin: пользователю <@{id}>')
-                embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
-                await ctx.send(embed=embed)
-            else:
-                embed = discord.Embed(colour=0x78ccf0, description=f'Нельзя передать монетки самому себе')
-                embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
-                await ctx.send(embed=embed)
-        else:
-            embed = discord.Embed(colour=0x78ccf0,
-                                  description=f'<@{id}> нет на сервере.\n\nНе удастся передать монетки')
-            embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
-            await ctx.send(embed=embed)
+    async def give(self, ctx, recipient_id, amount: int):
+        sender = BalanceCommands.get_sender(ctx)
+        sender_id = int(sender.id)
+
+        recipient_id = BalanceCommands.get_recipient_id(ctx=ctx, recipient_id_argument=recipient_id)
+
+        def try_give() -> str:
+            members = self.bot_data.get_members(ctx=ctx)
+            member_ids = MemberCommands.to_ids(members)
+            if recipient_id not in member_ids:
+                return f'<@{recipient_id}> нет на сервере.\n\nНе удастся передать монетки'
+
+            if recipient_id == sender_id:
+                return f'Нельзя передать монетки самому себе'
+
+            return f'Вы передали {amount} :coin: пользователю <@{recipient_id}>'
+
+        description = try_give()
+        await DiscordUtils.show_embed(
+            ctx,
+            colour=0x78ccf0,
+            description=description,
+            footer_text=sender.name,
+            footer_icon_url=sender.avatar_url,
+        )
 
     @give.error
     async def give_error(self, ctx, error):
         if isinstance(error, discord.ext.commands.CommandError):
+            print(error.args)
             embed = discord.Embed(colour=0xff2e2e, title='Не удалось передать монтеки',
                                   description='Укажите человека, его id или линк, а затем количество монеток,'
-                                              ' которое вы хотите передать')
+                                              f' которое вы хотите передать')
             await ctx.send(embed=embed)
 
     @commands.command()
