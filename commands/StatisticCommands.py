abdcb026574b248e8455caf6adae259679aa7e83
lizzzarrrd <polina.batkilina@gmail.com>
2022-04-21 22:08:58 +0300

    added Statistics commands class

---
+++ commands/StatisticCommands.py
@@ -0,0 +1,11 @@
+from discord.ext import commands
+from data.bot_data import BotData
+
+
+class StatisticCommands(commands.Cog):
+    def __init__(self, data: BotData):
+        self.bot_data = data
+
+    @commands.command
+    async def stats(self):
+        pass
