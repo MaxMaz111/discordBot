af36289b70c64d05ded7a8dcc2c5e8d207beb1d9
MaxMaz111 <maxmaz260206@gmail.com>
2022-04-22 15:10:18 +0300

    GuildData: get_member_with_id -> get_member

--- bot_commands/MembersCommands.py
+++ bot_commands/MembersCommands.py
@@ -32,39 +32,40 @@ class MemberCommands(commands.Cog):
         await ctx.send(nickname_str)
 
     @commands.command()
-    async def profile(self, ctx, id=None):
+    async def profile(self, ctx, discord_id=None):
         date_format = "%d.%m.%y"
-        if id is None:
+        if discord_id is None:
             sender = CommandUtils.get_author(ctx)
-            id = int(sender.id)
+            discord_id = int(sender.id)
         else:
-            id = CommandUtils.get_mentioned_id(ctx=ctx, recipient_id_argument=id)
+            discord_id = CommandUtils.get_mentioned_id(ctx=ctx, recipient_id_argument=discord_id)
 
         guild_id = self.data.get_guild_id(ctx=ctx)
         guild_obj = self.data.guild_id_to_data[guild_id]
-        user = guild_obj.get_member_with_id(id)
-        if user:
-            user = user[0]
-            user_nick = CommandUtils.to_nickname(user)
-            joined_at = user.joined_at.strftime(date_format)
-            created_at = user.created_at.strftime(date_format)
-            user_roles = filter(lambda x: x.name != '@everyone', user.roles)
-            user_name_roles = list(map(lambda x: f'`{x.name}`', user_roles))
-            user_img_url = user.avatar_url
-            author_nick = CommandUtils.to_nickname(ctx.author)
-            author_img_url = ctx.author.avatar_url
-
-            embed = discord.Embed(title=f'Информация о {user_nick}')
-            embed.set_thumbnail(url=user_img_url)
-            embed.add_field(name='Полное имя', value=user_nick)
-            embed.add_field(name='ID пользователя', value=user.id, inline=True)
-            embed.add_field(name='Присоединился к серверу', value=joined_at, inline=True)
-            embed.add_field(name='Аккаунт создан', value=created_at)
-            embed.add_field(name=f'Роли({len(user_name_roles)})', value=",".join(user_name_roles), inline=True)
-            embed.set_footer(text=f'Запросил(а) {author_nick}', icon_url=author_img_url)
-            await ctx.send(embed=embed)
-        else:
+        user = guild_obj.get_member(discord_id=discord_id)
+        if not user:
             await DiscordUtils.show_embed(ctx=ctx,
                                           colour=EmbedColor.ERROR,
-                                          title='Человек с таким ID или никнеймом нет на сервере'
+                                          title='Пользователя с таким ID или никнеймом нет на сервере'
                                           )
+            return
+
+        user_nick = CommandUtils.to_nickname(user)
+        joined_at = user.joined_at.strftime(date_format)
+        created_at = user.created_at.strftime(date_format)
+        user_roles = filter(lambda x: x.name != '@everyone', user.roles)
+        user_name_roles = list(map(lambda x: f'`{x.name}`', user_roles))
+        user_img_url = user.avatar_url
+        author_nick = CommandUtils.to_nickname(ctx.author)
+        author_img_url = ctx.author.avatar_url
+
+        embed = discord.Embed(title=f'Информация о {user_nick}')
+        embed.set_thumbnail(url=user_img_url)
+        embed.add_field(name='Полное имя', value=user_nick)
+        embed.add_field(name='ID пользователя', value=user.id, inline=True)
+        embed.add_field(name='Присоединился к серверу', value=joined_at, inline=True)
+        embed.add_field(name='Аккаунт создан', value=created_at)
+        embed.add_field(name=f'Роли({len(user_name_roles)})', value=",".join(user_name_roles), inline=True)
+        embed.set_footer(text=f'Запросил(а) {author_nick}', icon_url=author_img_url)
+        await ctx.send(embed=embed)
+
