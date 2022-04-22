8637cee3fa4750fa4888e6c1f368b5e61a8b174d
MaxMaz111 <maxmaz260206@gmail.com>
2022-04-22 18:16:38 +0300

    added types to StatisticCommands.py

--- bot_commands/StatisticCommands.py
+++ bot_commands/StatisticCommands.py
@@ -1,12 +1,17 @@
 from discord.ext import commands
+from discord.ext.commands import Context
 
 from data.bot_data import BotData
 
 
 class StatisticCommands(commands.Cog):
-    def __init__(self, data: BotData):
+    def __init__(self,
+                 data: BotData
+                 ):
         self.bot_data = data
 
     @commands.command
-    async def stats(self):
+    async def stats(self,
+                    ctx: Context
+                    ):
         pass
