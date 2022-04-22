3a4339621a76f59f29551ec21ce7d75005146eae
MaxMaz111 <maxmaz260206@gmail.com>
2022-04-22 12:30:39 +0300

    StatisticCommands: added StatisticType enum

--- bot_commands/StatisticCommands.py
+++ bot_commands/StatisticCommands.py
@@ -1,5 +1,11 @@
+from enum import Enum, auto
+
 from discord.ext import commands
 from data.bot_data import BotData
+
+
+class StatisticType(Enum):
+    BOT_COMMANDS_AMOUNT = auto()
 
 
 class StatisticCommands(commands.Cog):
