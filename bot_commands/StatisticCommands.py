4ac7bfc7f7ac24ae6302ad8f2c36f9ea64275723
MaxMaz111 <maxmaz260206@gmail.com>
2022-04-27 01:05:16 +0300

    stats shows user tag

--- bot_commands/StatisticCommands.py
+++ bot_commands/StatisticCommands.py
@@ -30,11 +30,12 @@ class StatisticCommands(commands.Cog):
         ]
 
         ans = [f'{statistic_readable} - {value}' for statistic_readable, value in statistics_readable]
+        ans.insert(0, f'<@{discord_id}>')
         description = '\n'.join(ans) if len(ans) > 0 else 'Ни одной статистики не собрано'
 
         await EmbedUtils.show_embed(ctx=ctx,
                                     colour=EmbedColor.ALL_OK,
-                                    title='Статиститика пользователя',
+                                    title=f'Статиститика пользователя',
                                     description=description,
                                     action_type=ActionType.ASKED,
                                     )
