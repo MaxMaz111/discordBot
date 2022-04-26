67ead3f15ea00e5baa10c103b704a6efe3436a92
MaxMaz111 <maxmaz260206@gmail.com>
2022-04-26 13:51:58 +0300

    StatisticCommands: added bot commands amount statistic triiger

--- bot_commands/StatisticCommands.py
+++ bot_commands/StatisticCommands.py
@@ -2,13 +2,24 @@ from discord.ext import commands
 from discord.ext.commands import Context
 
 from data.bot_data import BotData
+from data.models import StatisticType
 
 
 class StatisticCommands(commands.Cog):
     def __init__(self,
                  data: BotData,
                  ):
-        self.bot_data = data
+        self.data = data
+
+    @commands.before_invoke
+    async def register_call(self,
+                            ctx: Context,
+                            ):
+        self.data.update_user_statistic(
+            ctx=ctx,
+            statistic_type=StatisticType.BOT_COMMANDS_AMOUNT,
+            delta=1,
+        )
 
     @commands.command
     async def stats(self,
